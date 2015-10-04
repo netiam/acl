3cfa5d8715ae82965589eff1c406020637a8405d
Hannes Moser <hannes@impossiblearts.com>
2015-10-04 23:46:07 +0200

    feat: add constants module

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const PRIV_CREATE = 'C'
+export const PRIV_READ = 'R'
+export const PRIV_UPDATE = 'U'
+export const PRIV_DELETE = 'D'
+
+export const ALLOW = 'allow'
+export const DENY = 'deny'
+
+export const WILDCARD = '*'
